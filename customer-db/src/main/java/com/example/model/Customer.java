3d17b3c9aa13901b5d591906922fdbaf89d55c89
rajavnr2003 <help.nativechat@gmail.com>
2024-05-23 18:33:00 +0530

    first examples

---
+++ customer-db/src/main/java/com/example/model/Customer.java
@@ -0,0 +1,47 @@
+package com.example.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
+public class  Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(updatable = true)	
+	private String name;
+	
+	/*
+	 * // Default constructor public Customer() { }
+	 * 
+	 * public Customer(Long id, String name) { super(); this.id = id; this.name =
+	 * name; }
+	 */
+
+	// Default constructor
+    public Customer() {
+    }
+    
+    
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+}
