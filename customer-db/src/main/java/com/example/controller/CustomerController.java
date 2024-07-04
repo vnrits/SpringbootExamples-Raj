3d92a467dab54b5b6a3cd212e9c91f7d95678c4f
rajavnr2003 <help.nativechat@gmail.com>
2024-07-04 11:39:07 +0530

    more samples

--- customer-db/src/main/java/com/example/controller/CustomerController.java
+++ customer-db/src/main/java/com/example/controller/CustomerController.java
@@ -1,12 +1,13 @@
 package com.example.controller;
 
-import java.util.List;
+import com.example.model.Customer;
+import com.example.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.model.Customer;
-import com.example.repository.CustomerRepository;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CustomerController {
@@ -18,12 +19,43 @@ public class CustomerController {
 	public List<Customer> findAll() {
 		return repository.getAllCustomers();
 	}
+	
+	// working 
+		@RequestMapping("/listnative")
+		public List<Customer> findAllNative() {
+			return repository.findAll();
+		}
 
-	@RequestMapping("/one/{id}")
-	public Customer findOne(@PathVariable long id) {
+
+	@GetMapping("/getNative/{id}")
+	public Optional<Customer>  findOnevalue(@PathVariable long id) {
+		//return repository.getCustomerbyID(id);
+		return repository.findById(id);
+		//return Optional.ofNullable(repository.getCustomerbyID(id));
+	}
+
+		
+	@GetMapping("/getCustom/{id}")
+	public Customer find(@PathVariable long id) {
+		
 		return repository.getCustomerbyID(id);
 	}
 
+	@DeleteMapping("/deleteNative/{id}")
+	public ResponseEntity<String> deleteOnevalueN(@PathVariable long id) {
+		
+		 repository.deleteById(id);
+		 return new ResponseEntity<>("Customer found", HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteCustom/{id}")
+	public   ResponseEntity<String> deleteOneValueC(@PathVariable long id) {
+		
+		 repository.deleteOne(id);
+		 return new ResponseEntity<>("Customer found", HttpStatus.OK);
+	}
+	
+	
 	// Example with cURL_ curl -X POST http://localhost:8080/add -H 'Content-Type: application/json'   -d '{"id":"5","name":"Mark"}'
 	
 	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
--- customer-db/src/main/java/com/example/repository/CustomerRepository.java
+++ customer-db/src/main/java/com/example/repository/CustomerRepository.java
@@ -1,44 +1,28 @@
 
 package com.example.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
-
+import com.example.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.transaction.Transactional;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.stereotype.Repository;
-
-
-import com.example.model.Customer;
+import java.util.List;
 
 
 @Repository 
 public interface CustomerRepository extends JpaRepository<Customer, Long > {
  
-	@Query(value = "select * from customer where user_id=?1", nativeQuery = true)
+	@Query(value = "select * from customer where id=?1", nativeQuery = true)
 	  public  Customer  getCustomerbyID(Long id);
 	
 	@Query(value = "select * from customer", nativeQuery = true)
 	  public  List<Customer>  getAllCustomers();
 	
-	
+	@Modifying
+    @Transactional
+	@Query(value = "delete from customer where id=?1", nativeQuery = true)
+	  public  void  deleteOne(Long id);
 	
 }
